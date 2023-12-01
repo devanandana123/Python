bd5c764fc84cae5f82b666db25fab8f363b540b8
Devanandana S <147632168+devanandana123@users.noreply.github.com>
2023-12-01 10:35:19 +0530

    Add files via upload

---
+++ stringwithspace.py
@@ -0,0 +1,6 @@
+strA=input("enter the first string:")
+strB=input("enter the second string:")
+space=" "
+concatinate=space.join([strA,strB])
+print("concatunated string with space is:",concatinate)
+
