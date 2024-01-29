93867fe21edea2fb065f6f0bf93d3819bf235a69
Devanandana S <147632168+devanandana123@users.noreply.github.com>
2024-01-29 21:41:42 +0530

    Arithmetic.py

--- Arithmetic.py
+++ Arithmetic.py
@@ -1,6 +1,6 @@
 a=float(input("Enter the first number:"))
 b=float(input("Enter the second number:"))
-operation=("choose an arithmetic operations(*,/,-,+,//,%)")
+operation=input("choose an arithmetic operations(*,/,-,+,//,%)")
 if operation =="+":
     result=a+b
 elif operation =="-":
